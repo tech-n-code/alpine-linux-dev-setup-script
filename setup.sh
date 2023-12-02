4d653577e81f854feac3951e02d284bec4f76115
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-12-01 18:19:43 -0800

    alpine linux setup script for SW dev

---
+++ setup.sh
@@ -0,0 +1,23 @@
+!/bin/bash
+
+# Install and update repositories
+sed -i '/^#.*community/s/^#//' /etc/apk/repositories
+apk --no-cache update
+apk --no-cache upgrade
+
+# Install and configure Docker and Docker Compose
+apk --no-cache add docker docker-cli docker-compose
+addgroup root docker
+rc-update add docker
+rc-service docker start
+
+# Install and configure qemu-guest-agent
+apk --no-cache add qemu-guest-agent
+rc-update add qemu-guest-agent
+rc-service qemu-guest-agent start
+
+# Install git, htop, and nano
+apk --no-cache add git htop nano
+
+# Reboot the system
+reboot
